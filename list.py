36ebf5ddc3426c520118e3d45f737282bd160c06
wangui-wanyiri <ritahwanyiri@gmail.com>
2024-11-20 01:19:05 +0300

    python list assignment

---
+++ list.py
@@ -0,0 +1,25 @@
+#creating a list
+my_list=[]
+
+#Methods used to manipulate items in a list
+
+# adding items to my list using append
+my_list.append (10)
+my_list.append (20)
+my_list.append (30)
+my_list.append (40)
+
+# inserting a value in the list
+my_list.insert(1, 15)
+
+#extending the list
+my_list.extend([50, 60, 70])
+
+#sorting the list in ascending order
+my_list.sort()
+
+#Displaying an index of a value
+print(my_list[3])
+
+
+
